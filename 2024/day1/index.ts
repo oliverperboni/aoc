0ac70c90cff968a6c287e69dfa9aa2013303f366
oliverperboni <oliverperboni@gmail.com>
2024-12-01 21:28:45 +0000

    feat: aoc day 1 part 2

--- 2024/day1/index.ts
+++ 2024/day1/index.ts
@@ -18,10 +18,23 @@ stack1.sort()
 
 const length = stack0.length
 let sum = 0;
+let sumPart2 = 0;
+function countOccurrences(array: number[], element: number): number {
+  return array.filter(item => item === element).length;
+}
+for (let index = 0; index < stack0.length; index++) {
+  const element = stack0[index];
+  const numberOfOccurencies = countOccurrences(stack1,element)
+  sumPart2 = sumPart2 + element*numberOfOccurencies
+}
+
 for (let i = 0; i < length; i++) {
     const element0 = stack0.pop()!;
     const element1 = stack1.pop()!;
     element0 > element1  ? sum = sum + element0 - element1 : sum = sum + element1 - element0
     
 }
-console.log("the awnser is:",sum)
+ console.log("the awnser for part-1:",sum)
+console.log("the awnser for part-2",sumPart2);
+
+
