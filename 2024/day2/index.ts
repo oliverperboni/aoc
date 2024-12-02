efc14a47c6761451e435b32e7faff7284a47996d
oliverperboni <oliverperboni@gmail.com>
2024-12-02 19:42:27 +0000

    feat: day 2 part 2

--- 2024/day2/index.ts
+++ 2024/day2/index.ts
@@ -26,9 +26,18 @@ const isSafe = (array: string[]): boolean => {
 
   return true;
 };
+const isSafeWithTolerance = (array: string[]): boolean => {
+    for (let i = 0; i < array.length; i++) {
+        const modifiedArray = [...array.slice(0, i), ...array.slice(i + 1)];
+        if (isSafe(modifiedArray)) {
+          return true; 
+        }
+      }
+      return false
+};
 
 content.split("\n").forEach((line) => {
   const newLine = line.trim().split(" ");
-  if (isSafe(newLine)) numberOfSafes += 1;
+  if (isSafeWithTolerance(newLine)) numberOfSafes += 1;
 });
 console.log(numberOfSafes);
