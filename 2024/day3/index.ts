e5a38439ddc57bd84fbc2ecf8564fd5d53d0bead
oliverperboni <oliverperboni@gmail.com>
2024-12-03 16:53:10 +0000

    feat: day 3 part 2

--- 2024/day3/index.ts
+++ 2024/day3/index.ts
@@ -3,10 +3,28 @@ const file = Bun.file("./input.txt");
 const content = await file.text();
 
 const regex = /mul\((\d+),(\d+)\)/g;
-let sumPart1 = 0
+let sumPart1 = 0;
 const match = Array.from(content.matchAll(regex));
-match.forEach((pair)=> {
-    sumPart1 += Number(pair[1]) * Number(pair[2])
+match.forEach((pair) => {
+  sumPart1 += Number(pair[1]) * Number(pair[2]);
 });
 
-console.log("awnser for the first part is:",sumPart1)
+const regex2 = /(mul\((\d+),(\d+)\))|(do\(\))|(don't\(\))/g;
+let sumPart2 = 0;
+let enable = true; 
+const matchesPart2 = Array.from(content.matchAll(regex2));
+
+matchesPart2.forEach((match) => {
+  if (match[4]) { 
+    enable = true;
+  } else if (match[5]) { 
+    enable = false;
+  } else if (match[1] && enable) { 
+    const a = Number(match[2]);
+    const b = Number(match[3]);
+    sumPart2 += a * b;
+  }
+});
+
+console.log("Answer for the first part is:", sumPart1);
+console.log("Answer for the second part is:", sumPart2);
